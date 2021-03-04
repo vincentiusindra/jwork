36e03fdf72d9dd4d83bcf4cbd6c3045466cac6b5
Vincentius Indra Lithgow <vincentiuslithgow@gmail.com>
2021-03-04 17:12:28 +0700

    modul 1

---
+++ jwork/Recruiter.java
@@ -0,0 +1,70 @@
+
+/**
+ * Write a description of class Recruiter here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Recruiter
+{
+    // instance variables - replace the example below with your own
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String joinDate;
+
+    /**
+     * Constructor for objects of class Recruiter
+     */
+    public Recruiter()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static int getId()
+    {
+        return 0;
+    }
+    
+    public static String getName()
+    {
+        return "oop";
+    }
+    
+    public static String getEmail()
+    {
+        return "oop";
+    }
+    
+    public static String getPhoneNumber()
+    {
+        return "oop";
+    }
+    
+    public static void setid(int id)
+    {
+
+    }
+    
+    public static void setEmail(String Email)
+    {
+
+    }
+    
+    public static void setName(String Name)
+    {
+
+    }
+    
+    public static void setPhoneNumber(String phoneNumber)
+    {
+        
+    }
+}
