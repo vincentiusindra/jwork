444edd4124e34af61bdbcf8d4d6750b8458e3527
Vincentius Indra Lithgow <vincentiuslithgow@gmail.com>
2021-04-03 09:23:58 +0700

    Modul 4 - Post Test - Revisi 1

--- jwork/JWork.java
+++ jwork/JWork.java
@@ -17,8 +17,8 @@ public class JWork
         Recruiter recruiter_1 = new Recruiter(123, "Vincentius Indra Lithgow", "vincentiuslithgow@gmail.com", "088219440870", location_1);
         Job job_1 = new Job(123, "Manager", recruiter_1, 10000, JobCategory.UI);
         Jobseeker jobseeker_1 = new Jobseeker(321, "Budi", "budi@gmail.com", "notapassword", "20/2/2020");
-        BankPayment test_1 = new BankPayment(224, job_1, "3/4/2020", jobseeker_1, InvoiceStatus.Finished, 0);
-        BankPayment test_2 = new BankPayment(224, job_1, "3/4/2020", jobseeker_1, InvoiceStatus.Finished, 1000);
+        BankPayment test_1 = new BankPayment(024, job_1, "3/4/2020", jobseeker_1, InvoiceStatus.Finished, 0);
+        BankPayment test_2 = new BankPayment(024, job_1, "3/4/2020", jobseeker_1, InvoiceStatus.Finished, 1000);
         test_1.printData();
         test_2.printData();
     }
